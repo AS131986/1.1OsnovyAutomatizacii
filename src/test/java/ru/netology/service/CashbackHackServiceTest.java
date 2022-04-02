36012b89b143ce32de28dc1073b3ca0bb5c203d4
Alexander <AlexanderSGSklyar@mail.ru>
2022-04-02 14:29:09 +0300

    Add testng, CashbackHackService, CashbackHackServiceTest

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void remainBefore500() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 500;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void remainBefore999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void remainBefore1() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void remainBefore1000Bug() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+}
